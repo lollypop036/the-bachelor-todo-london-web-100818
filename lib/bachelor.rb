2c784bf7145b79c1dbde3bdc54ccbb9a8ff51fcc
Lola Onanuga <slonanuga@gmail.com>
2018-10-03 20:52:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -67,7 +67,7 @@ def get_average_age_for_season(data, season)
         
     end
   
-   x = age_array.inject(0) { |sum, x| sum += x } / age_array.size 
-  x.round
+   x = age_array.inject(0) { |sum, x| sum += x } 
+  x/age_array.size
   
 end
