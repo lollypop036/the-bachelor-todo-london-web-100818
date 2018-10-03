6b4e6a149668219ef839f5a8960520dc3eb50713
Lola Onanuga <slonanuga@gmail.com>
2018-10-03 20:21:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,74 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+
+  data[season].each do |girls|
+    
+    if girls["status"] == "Winner"
+      
+       return girls["name"].split(" ")[0]
+       
+    end
+  end
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
+   data.each do |szn, girls|
+     
+     girls.each do |info|
+       
+      if info["occupation"] == occupation
+       return info["name"]
+      end
+     end
+   end
+   
+   
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ count = 0
+ data.each do |szn, girls|
+     
+     girls.each do |info|
+       
+      if info["hometown"] == hometown
+       count +=1
+      end
+     end
+   end
+   count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  
+   data.each do |szn, girls|
+     
+     girls.find do |info|
+       
+      if info["hometown"] == hometown
+       return info["occupation"]
+      end
+     end
+   end
+   
+   
 end
+  
 
 def get_average_age_for_season(data, season)
-  # code here
+  
+  age_array = []
+  
+    data[season].each do |girls|
+        age_array.push(girls["age"].to_i)
+        
+    end
+  
+    age_array.inject(0) { |sum, x| sum += x } / age_array.size 
+ 
+
+  
 end
